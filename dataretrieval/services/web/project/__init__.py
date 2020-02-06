11ec618f462e9a1ec12d93db28e894123edaceb0
asimazmi <asimazmi@iu.edu>
2020-02-06 14:59:31 -0500

    #7 nextrad included in requirements file. Will be installed automatically

--- dataretrieval/services/web/project/__init__.py
+++ dataretrieval/services/web/project/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask, jsonify
-
+import nexradaws
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return jsonify(hello="world")
+	conn = nexradaws.NexradAwsInterface()
+	years =  conn.get_avail_years()
+	print(years)
+	return jsonify(hello="world")
