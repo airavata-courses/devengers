a6dee61cb2a75688475fd34a1b24413cc98fbf59
asimazmi <asimazmi@iu.edu>
2020-02-06 19:10:41 -0500

    #6 postgres included in the environment and database created

--- dataretrieval/services/web/project/__init__.py
+++ dataretrieval/services/web/project/__init__.py
@@ -1,7 +1,22 @@
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 import nexradaws
 
 app = Flask(__name__)
+app.config.from_object("project.config.Config")
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = "dataretrieval_tracker"
+	 
+    id = db.Column(db.Integer, primary_key=True)
+    userid = db.Column(db.Integer)
+    correlationid = db.Column(db.Integer)
+    request = db.Column(db.String)
+    status =  db.Column(db.String)
+
+    def __init__(self, email):
+        self.email = email
 
 
 @app.route("/")
