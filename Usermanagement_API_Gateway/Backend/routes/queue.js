db147178d50bbcdcf7e99afb46c0e6931b295537
Suyash Poredi <suyashporedi01@gmail.com>
2020-04-23 22:31:05 -0400

    initial

--- Usermanagement_API_Gateway/Backend/routes/queue.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { requireSignin } = require("../controllers/auth");
-const router = express.Router();
-const { sendMsg , receiveMsg}=require('../controllers/queue');
-
-router.put("/weatherData/queue", requireSignin, sendMsg ,receiveMsg);
-
-module.exports = router;
-
